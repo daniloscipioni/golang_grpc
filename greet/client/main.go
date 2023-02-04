d20e97d8ea1c927c7d94b7bbac0aec0ca454605d
Danilo Eduardo <daniloeduardo.ciandt@bexspay.com.br>
2023-02-04 09:38:01 -0300

    SSL implementation but not running cause ssl certification problem

--- greet/client/main.go
+++ greet/client/main.go
@@ -2,17 +2,34 @@ package main
 
 import (
 	"log"
-	"time"
+	"path/filepath"
 
 	pb "github.com/daniloscipioni/grpc/greet/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 )
 
 var addr string = "localhost:50051"
 
 func main() {
-	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	tls := true
+	opts := []grpc.DialOption{}
+
+	if tls {
+		certFile, _ := filepath.Abs("greet/ssl/ca.crt")
+
+		creds, err := credentials.NewClientTLSFromFile(certFile, "")
+
+		if err != nil {
+			log.Fatalf("Error while loading CA trust certificate: %v\n", err)
+		}
+
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+
+	}
+
+	conn, err := grpc.Dial(addr, opts...)
 
 	if err != nil {
 		log.Fatalf("Failed to connect: %v\n", err)
@@ -22,11 +39,11 @@ func main() {
 
 	c := pb.NewGreetServiceClient(conn)
 
-	//doGreet(c)
+	doGreet(c)
 	//doGreetManyTimes(c)
 	//doGreetWithDeadline(c, 1*time.Second)
 	//doGreetWithDeadline(c, 2*time.Second)
 	//doGreetWithDeadline(c, 3*time.Second)
-	doGreetWithDeadline(c, 4*time.Second)
+	//doGreetWithDeadline(c, 4*time.Second)
 	//doGreetWithDeadline(c, 5*time.Second)
 }
--- greet/server/main.go
+++ greet/server/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"log"
 	"net"
+	"path/filepath"
 
 	pb "github.com/daniloscipioni/grpc/greet/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 var addr string = "0.0.0.0:50051"
@@ -23,7 +25,23 @@ func main() {
 
 	log.Printf("Listening on %s\n", addr)
 
-	s := grpc.NewServer()
+	opts := []grpc.ServerOption{}
+	tls := true
+
+	if tls {
+		certFile, _ := filepath.Abs("greet/ssl/server.crt")
+		keyFile, _ := filepath.Abs("greet/ssl/server.key")
+
+		creds, err := credentials.NewServerTLSFromFile(certFile, keyFile)
+
+		if err != nil {
+			log.Fatalf("Failed loading certificates: %v\n", err)
+		}
+
+		opts = append(opts, grpc.Creds(creds))
+	}
+
+	s := grpc.NewServer(opts...)
 	pb.RegisterGreetServiceServer(s, &Server{})
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("Failed to serve: %v\n", err)
